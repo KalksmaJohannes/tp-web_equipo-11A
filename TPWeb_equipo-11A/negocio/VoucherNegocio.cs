80b0d267f1aec7563531f06957587c443793e743
MerinaPinti <marinacelestepinti2@gmail.com>
2025-05-09 02:04:38 -0300

    Se creó la clase Voucher Negocio

---
+++ TPWeb_equipo-11A/negocio/VoucherNegocio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class VoucherNegocio
+    {
+        public bool existeVoucher(string codigo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT COUNT(*) FROM VOUCHERS WHERE CodigoVoucher = @codigo");
+                datos.setearParametros("@codigo", codigo);
+
+                int cantidad = datos.ejecutarScalar();
+                return cantidad > 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+    }
+}
