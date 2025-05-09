2c8a02fd4d9cafc7d6968d6a2f058937757cf783
MerinaPinti <marinacelestepinti2@gmail.com>
2025-05-09 01:15:35 -0300

    Cambio de nombre de la pag "default"

---
+++ TPWeb_equipo-11A/TPWeb_equipo-11A/ListadoArt.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="ListadoArt.aspx.cs" Inherits="TPWeb_equipo_11A.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true"></asp:GridView>
+    <h2>PRUEBA</h2>
+</asp:Content>
---
+++ TPWeb_equipo-11A/TPWeb_equipo-11A/ListadoArt.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace TPWeb_equipo_11A
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private List<Articulo> lista;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                cargar();
+            }
+        }
+
+        public void cargar()
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            GridView1.DataSource = negocio.listar();
+            GridView1.DataBind();
+        }
+
+    
+    }
+}
