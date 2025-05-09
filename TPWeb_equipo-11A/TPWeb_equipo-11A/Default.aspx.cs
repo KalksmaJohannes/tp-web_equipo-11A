6943a87479059ed1f51ceaaa1568761da5b01d6c
MerinaPinti <marinacelestepinti2@gmail.com>
2025-05-09 02:05:17 -0300

    Se creó el método validar para el número de voucher
    Lanza alerta si el código no existe.

--- TPWeb_equipo-11A/TPWeb_equipo-11A/Default.aspx.cs
+++ TPWeb_equipo-11A/TPWeb_equipo-11A/Default.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace TPWeb_equipo_11A
 {
@@ -12,6 +14,22 @@ namespace TPWeb_equipo_11A
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            string codigo = voucherInput.Text.Trim();
+            VoucherNegocio negocio = new VoucherNegocio();
+
+            if (!negocio.existeVoucher(codigo))
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "alerta", "window.onload = function() { alert('¡Ups! Código inexistente'); document.getElementById('" + voucherInput.ClientID + "').focus(); };", true);
+            }
+            else
+            {
+                // Código válido
+                //SI EXISTE EL VOUCHER SE DEBE REDIRIGIR A LA PÁGINA QUE CONTENGA LOS PRODUCTOS QUE SE PUEDEN CANJEAR 
+            }
         }
     }
 }
