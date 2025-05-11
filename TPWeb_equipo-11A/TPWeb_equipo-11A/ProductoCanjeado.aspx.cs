f4b166bf74a28ca56670969f16adf1798ae42230
MerinaPinti <marinacelestepinti2@gmail.com>
2025-05-11 20:48:44 -0300

    Se crea nueva página producto Canjeado
    Se obtiene con un session el "articulo seleccionado", y se visualiza la lista con el artículo en cuestión. Se utiliza data bound para utilizar el repeater

---
+++ TPWeb_equipo-11A/TPWeb_equipo-11A/ProductoCanjeado.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace TPWeb_equipo_11A
+{
+    public partial class ProductoCanjeado : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["articuloSeleccionado"] != null)
+                {
+                    int id = int.Parse(Session["articuloSeleccionado"].ToString());
+                    ArticuloNegocio negocio = new ArticuloNegocio();
+                    Articulo articulo = negocio.obtenerPorId(id);
+
+                    // Creamos una lista porque el Repeater espera una colección
+                    List<Articulo> lista = new List<Articulo> { articulo };
+                    RepeaterProducto.DataSource = lista;
+                    RepeaterProducto.DataBind();
+                }
+            }
+        }
+
+        protected void RepeaterProducto_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Articulo articulo = (Articulo)e.Item.DataItem;
+                Repeater repeaterImagen = (Repeater)e.Item.FindControl("RepeaterImagen");
+                repeaterImagen.DataSource = articulo.Imagen;
+                repeaterImagen.DataBind();
+            }
+        }
+    }
+    }
